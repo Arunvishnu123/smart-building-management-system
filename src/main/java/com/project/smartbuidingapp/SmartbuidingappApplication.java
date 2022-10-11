8bb29bb0013546032a0e4dd50e7b905e0f0eb16f
Arun R S <arunvishnu40@gmail.com>
2022-10-11 10:02:28 +0200

    first commit

---
+++ src/main/java/com/project/smartbuidingapp/SmartbuidingappApplication.java
@@ -0,0 +1,13 @@
+package com.project.smartbuidingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbuidingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartbuidingappApplication.class, args);
+	}
+
+}
