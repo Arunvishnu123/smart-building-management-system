4d5129d1ca936453c59fc663266847db14d059ad
Arun R S <arunvishnu40@gmail.com>
2022-11-02 12:31:59 +0100

    authorization filter added

--- src/main/java/com/project/smartbuidingapp/Security/SecurityConfiguration.java
+++ src/main/java/com/project/smartbuidingapp/Security/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,6 +17,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.http.HttpMethod.*;
@@ -46,6 +48,7 @@ public class SecurityConfiguration {
                http.authorizeRequests().antMatchers(DELETE, "/api/v1/**").hasAnyAuthority("USER");
                http.authorizeRequests().anyRequest().permitAll();
                http.addFilter(new CustomAuthenticationFilter(authManagerBuilder.getOrBuild()));
+               http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
