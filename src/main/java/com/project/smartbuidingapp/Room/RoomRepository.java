4d11c5325b0948936ec78c9149373264bcd58193
Arun R S <arunvishnu40@gmail.com>
2022-11-03 16:36:20 +0100

    final

--- src/main/java/com/project/smartbuidingapp/Room/RoomRepository.java
+++ src/main/java/com/project/smartbuidingapp/Room/RoomRepository.java
@@ -1,4 +1,14 @@
 package com.project.smartbuidingapp.Room;
 
-public class RoomRepository {
+import com.project.smartbuidingapp.Heater.HeaterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RoomRepository extends JpaRepository<RoomEntity, Long> {
+
+    //list all rooms in the same building
+    List<RoomEntity> findByBuildingID(Long buildingID);
 }
